6118aa9c8d77d4de8a4fe0080960da27ef3e4b37
jooapas <jooa.akonpelto@gmail.com>
2023-01-15 22:50:38 +0200

    website

--- MustatRenkaat/js.js
+++ MustatRenkaat/js.js
@@ -1,7 +1,3 @@
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").style.fontSize = "30px";
-    } else {
-      document.getElementById("header").style.scale = 12
-    }
-  }
+function ShowSearch(){
+
+}
