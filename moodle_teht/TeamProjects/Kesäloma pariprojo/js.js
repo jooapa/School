9672a92f4fc1bcd5ddfbc570d8d916d258484780
jooapa <jooa.akonpelto@gmail.com>
2023-08-09 18:24:56 +0300

    d

--- moodle_teht/TeamProjects/Kesäloma pariprojo/js.js
+++ moodle_teht/TeamProjects/Kesäloma pariprojo/js.js
@@ -26,14 +26,3 @@ scrollBtn.addEventListener("click", function(){
         behavior: "smooth"
     });
 });
-
-
-document.getElementById("body").onscroll = function myFunction() {
-  var scrolltotop = document.scrollingElement.scrollTop;
-  var target = document.getElementById("main1");
-  var xvalue = "center";
-  var factor = 0.5;
-  var yvalue = scrolltotop * factor;
-  target.style.backgroundPosition = xvalue + " " + yvalue + "px";
-};
-
