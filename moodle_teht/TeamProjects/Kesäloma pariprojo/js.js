282032c8917eadd988b9f97c06943c48be8b9eda
jooapa <37744313+jooapa@users.noreply.github.com>
2023-08-09 11:50:00 +0300

    i

---
+++ moodle_teht/TeamProjects/Kesäloma pariprojo/js.js
@@ -0,0 +1,25 @@
+//onclick button id="JooaBtn addEventListener
+
+var jooaBtn = document.getElementById("JooaBtn");
+var MikoBtn = document.getElementById("MikoBtn");
+var scrollBtn = document.getElementById("ScrollUp");
+
+jooaBtn.addEventListener("click", function(){
+    //hide div id="Miko" and show div id="Jooa"
+    // document.getElementById("Miko").style.display = "none";
+    document.getElementById("jooa").style.display = "block";
+    document.getElementById("miko").style.display = "none";
+    document.getElementById("hideShow").style.display = "flex";
+});
+
+MikoBtn.addEventListener("click", function(){
+    //hide div id="Jooa" and show div id="Miko"
+    document.getElementById("jooa").style.display = "none";
+    document.getElementById("miko").style.display = "block";
+    document.getElementById("hideShow").style.display = "flex";
+});
+
+scrollBtn.addEventListener("click", function(){
+    //scroll to top
+    window.scrollTo(0, 0);
+});
