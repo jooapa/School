e41771467f7f8bdcfbbf173b74580e3fb99b49d0
jooapa <37744313+jooapa@users.noreply.github.com>
2023-08-29 09:55:55 +0300

    Bonus tehtävä - Muistiinpanosovellus

--- moodle_teht/react/react_teht/src/App.jsx
+++ moodle_teht/react/react_teht/src/App.jsx
@@ -1,50 +1,66 @@
 import { useState } from 'react'
 import './App.css'
 
-const Laskuri = () => {
-  const [num, setNum] = useState(0)
-
-  return (
-    <div className="App">
-      <h1>Interaktiivinen laskuri</h1>
-      <p>{num}</p>
-      <button onClick={() => setNum(num + 1)}>Lisää</button>
-      <button onClick={() => setNum(num - 1)}>Vähennä</button>
-    </div>
-  )
-}
-
-const TehtäväLista = () => {
+const MuistiinPano = () => {
   const [tasks, setTasks] = useState([])
   const [task, setTask] = useState('')
-  
+  const [taskHeader, setTaskHeader] = useState('')
+  const [taskHeaders, setTaskHeaders] = useState([])
+
   const addTask = () => {
-    if (task === '') return
-    setTasks([...tasks, task])
-    setTask('')
-  }
+    if (task === "") return;
+    if (taskHeader === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+    setTaskHeaders([...taskHeaders, taskHeader]);
+    setTaskHeader("");
+  };
 
   const removeTask = (index) => {
     const newTasks = [...tasks]
+    const newTaskHeaders = [...taskHeaders]
     newTasks.splice(index, 1)
+    newTaskHeaders.splice(index, 1)
+    setTasks(newTasks)
+    setTaskHeaders(newTaskHeaders)
+  }
+
+  const handleUpdate = (e, index) => {
+    const newTasks = [...tasks]
+    newTasks[index] = e.target.value
     setTasks(newTasks)
   }
 
   return (
     <div className="App">
       <h1>Tehtävälista</h1>
+      {"Otsikko: "}
+      <input
+        type="text"
+        value={taskHeader}
+        onChange={(e) => setTaskHeader(e.target.value)}
+      />
+      <br />
+      {"Tehtävä: "}
       <input
         type="text"
         value={task}
         onChange={(e) => setTask(e.target.value)}
       />
+      <br />
       <button onClick={addTask}>Lisää</button>
-      
-        {tasks.map((task, index) => (
-          <p key={index}>
-            <b>{task}</b> <button onClick={() => removeTask(index)}>Poista: {task}</button>
-          </p>
-        ))}
+      <div>
+        <h2>Tehtävät</h2>
+        <div className="MuistikorttiContainer">
+          {tasks.map((task, index) => (
+            <div className="Muistikortti" key={index}>
+              <h3 contentEditable>{taskHeaders[index]}</h3>
+              <p contentEditable>{task}</p>
+              <button onClick={() => removeTask(index)}>Poista</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
@@ -52,8 +68,7 @@ const TehtäväLista = () => {
 function App() {
   return (
     <div className="App">
-      <Laskuri />
-      <TehtäväLista />
+      <MuistiinPano />
     </div>
   );
 }
